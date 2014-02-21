ec5567b31e80ff41557cd71ad529297976f3eaef
Nick Lehmann <nickmann97@gmail.com>
2014-02-21 11:42:37 +0100

    Add solution for problem 3

--- Python/mathe.py
+++ Python/mathe.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 # my own module with some functions for solving mathematical problems
+
+# finds the highest prime factor of the argument in a recursive way
+def highest_prime(n):
+    i = 2
+    while(n % i != 0 and i < n):
+        i += 1
+    if(i < n):
+        return highest_prime(n/i)
+    else:
+        return n
---
+++ Python/problem3.py
@@ -0,0 +1,3 @@
+from mathe import highest_prime
+
+print highest_prime(600851475143)
